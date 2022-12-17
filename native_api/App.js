49fe88fc3227b6c3e54669b4f90a7cf929b81bc9
onely6721 <aesamokish@gmail.com>
2022-12-17 19:33:41 +0200

    add storage

--- native_api/App.js
+++ native_api/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
 
-import { NavigationContainer } from '@react-navigation/native';
-import UniversityList from "./pages/UniversityList/UniversityList";
-import UniversityDetail from "./pages/UniversityDetail/UniversityDetail";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import UniversityList from './pages/UniversityList/UniversityList';
+import UniversityDetail from './pages/UniversityDetail/UniversityDetail';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="UniversityList" component={UniversityList} />
-                <Stack.Screen name="UniversityDetail" component={UniversityDetail} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="UniversityList" component={UniversityList} />
+        <Stack.Screen name="UniversityDetail" component={UniversityDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
-
 
 export default App;
--- native_api/pages/UniversityList/UniversityList.js
+++ native_api/pages/UniversityList/UniversityList.js
@@ -1,37 +1,71 @@
 import React, {useEffect, useState} from 'react';
-import axios from "axios";
-import {ScrollView, Text} from "react-native";
+import axios from 'axios';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import storage from '../../storage/Storage';
 
-const API_URL = 'http://universities.hipolabs.com/search'
+const API_URL = 'http://universities.hipolabs.com/search';
 
 const UniversityList = ({navigation}) => {
-    const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        const getData = async () => {
-            const result = await axios.get(API_URL)
-            setData(result?.data.filter((univ) => univ.country === 'Ukraine'))
-        }
+  useEffect(() => {
+    const getData = async () => {
+      const result = await axios.get(API_URL);
+      if (result.data.length > 0) {
+        const ukUniversity = result?.data.filter(
+          univ => univ.country === 'Ukraine',
+        );
+        setData(ukUniversity);
+        storage.save({
+          key: 'University',
+          data: {
+            ukUniversity,
+          },
+          expires: 1000 * 3600,
+        });
+      } else {
+        storage
+          .load({
+            key: 'University',
+            autoSync: true,
+            syncInBackground: true,
+            syncParams: {
+              someFlag: true,
+            },
+          })
+          .then(res => {
+            setData(res);
+          });
+      }
+    };
 
-        getData().then()
-    }, [])
+    getData().then();
+  }, []);
 
-    const onUniversityPress = (university) => {
-        navigation.navigate('UniversityDetail', {university})
-    }
+  const onUniversityPress = university => {
+    navigation.navigate('UniversityDetail', {university});
+  };
 
-    return (
-        <ScrollView>
-            {data?.map((university) =>
-                <Text
-                    onPress={() => onUniversityPress(university)}
-                >
-                    {university.name}
-                </Text>
-            )}
-
-        </ScrollView>
-    );
+  return (
+    <ScrollView>
+      {data?.map(university => (
+        <View style={styles.text} key={university.name}>
+          <Text onPress={() => onUniversityPress(university)}>
+            {university.name}
+          </Text>
+        </View>
+      ))}
+    </ScrollView>
+  );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    borderRadius: 1,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderTopColor: 'white',
+  },
+});
 
 export default UniversityList;
